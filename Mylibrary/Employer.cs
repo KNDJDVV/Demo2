1f7b2d66adf653821c2a05d1fae408d62ab242bf
Juan Valencia <vjuandavid03@gmail.com>
2024-10-09 22:23:27 -0500

    Abstraccion parte 1

---
+++ Mylibrary/Employer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mylibrary
+{
+    public abstract class Employer
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        public abstract decimal CalculateMonthlyPayment();
+        
+
+        }
+    }
+
+    public class Secretary : Employer
+    {
+
+    }
+
+    public class Manager : Employer 
+    {
+    
+    }
+}
