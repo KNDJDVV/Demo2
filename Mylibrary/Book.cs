be39de41f9c32509866875fb35d408d516843b82
Juan Valencia <vjuandavid03@gmail.com>
2024-10-09 22:17:21 -0500

    Estilo Nuevo y Estilo Viejo de Constructor

---
+++ Mylibrary/Book.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Mylibrary
+{
+    public class Book : Product
+    {
+        public string Title {  get; set; } 
+        public Book(int id, string name, decimal unitPrice, int unitsInStock, String title) :
+        base (id,name,unitPrice,unitsInStock)
+        {
+            Title = title;
+        }
+    }
+
+    public class Book2(int id, string name, decimal unitPrice, int unitsInStock, String title) :
+    Product(id, name, unitPrice, unitsInStock)
+    {
+        public string Title => Title;
+      
+    }
+}
